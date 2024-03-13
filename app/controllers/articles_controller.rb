4a64f539a943a9226fd7f489cac04a4c51098e8e
No-YE <nye7181@gmail.com>
2024-03-13 21:52:29 +0900

    add article show page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,15 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[show]
+
   def index
     @pagy, @articles = pagy(Article.kept)
+  end
+
+  def show; end
+
+  private
+
+  def set_article
+    @article = Article.kept.published.find(params[:id])
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
 module ApplicationHelper
   include Pagy::Frontend
+
+  def markdown(text)
+    Markdown.new(text).to_html.html_safe
+  end
 end
